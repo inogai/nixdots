15a094c3ed7d4b46dbb83d3aef7d7b791a1a5bd8
inogai <inogai.sign@gmail.com>
2025-11-20 15:02:18 +0800

    ref(aerospace): migrate to home manager module

--- modules/aerospace/default.nix
+++ modules/aerospace/default.nix
@@ -7,9 +7,6 @@
   toJankyBordersColor = color: "0xff${color}"; # 0xff<color>
 in {
   home.packages = with pkgs; [
-    bash
-    aerospace
-    jankyborders
     sbar-inogai
   ];
 
@@ -24,131 +21,118 @@ in {
     };
   };
 
-  home.file.".config/aerospace/aerospace.toml" = {
-    text = ''
-      # https://nikitabobko.github.io/AeroSpace/commands
-      # https://nikitabobko.github.io/AeroSpace/guide
-
-      after-login-command = []
+  programs.aerospace = {
+    enable = true;
+    userSettings = {
+      after-login-command = [];
 
       after-startup-command = [
-        'exec-and-forget sbar-inogai',
-      ]
+        "exec-and-forget sbar-inogai"
+      ];
 
       exec-on-workspace-change = [
-        '${pkgs.bash}/bin/bash',
-        '-c',
-        'sbar-inogai --trigger aerospace_workspace_change AEROSPACE_FOCUSED_WORKSPACE=$AEROSPACE_FOCUSED_WORKSPACE AEROSPACE_PREV_WORKSPACE=$AEROSPACE_PREV_WORKSPACE'
-      ]
+        "${pkgs.bash}/bin/bash"
+        "-c"
+        "sbar-inogai --trigger aerospace_workspace_change AEROSPACE_FOCUSED_WORKSPACE=$AEROSPACE_FOCUSED_WORKSPACE AEROSPACE_PREV_WORKSPACE=$AEROSPACE_PREV_WORKSPACE"
+      ];
 
-      start-at-login = true
+      start-at-login = true;
+      enable-normalization-flatten-containers = true;
+      enable-normalization-opposite-orientation-for-nested-containers = true;
+      accordion-padding = 30;
+      default-root-container-layout = "tiles";
+      default-root-container-orientation = "auto";
+      on-focused-monitor-changed = ["move-mouse monitor-lazy-center"];
+      automatically-unhide-macos-hidden-apps = true;
 
-      enable-normalization-flatten-containers = true
-      enable-normalization-opposite-orientation-for-nested-containers = true
+      key-mapping.preset = "qwerty";
 
-      accordion-padding = 30
+      gaps = {
+        inner.horizontal = 12;
+        inner.vertical = 8;
+        outer.left = 8;
+        outer.bottom = 8;
+        outer.top = 26;
+        outer.right = 8;
+      };
 
-      default-root-container-layout = 'tiles'
-      default-root-container-orientation = 'auto'
+      mode = {
+        "main" = {
+          binding = {
+            alt-slash = "layout tiles accordion";
+            alt-comma = "layout horizontal vertical";
+            alt-h = "focus left";
+            alt-j = "focus down";
+            alt-k = "focus up";
+            alt-l = "focus right";
+            "alt-shift-h" = "move left";
+            "alt-shift-j" = "move down";
+            "alt-shift-k" = "move up";
+            "alt-shift-l" = "move right";
+            alt-minus = "resize smart -50";
+            alt-equal = "resize smart +50";
+            "alt-shift-minus" = "balance-sizes";
+            "alt-shift-equal" = "balance-sizes";
+            alt-backtick = "focus-monitor --wrap-around next";
+            "alt-shift-backtick" = [
+              "move-node-to-monitor --wrap-around next"
+              "focus-monitor --wrap-around next"
+            ];
+            alt-tab = "exec-and-forget fzfmenu -q=\"fo \"";
+            alt-1 = "workspace 1";
+            alt-2 = "workspace 2";
+            alt-3 = "workspace 3";
+            alt-w = "workspace W";
+            alt-e = "workspace E";
+            alt-a = "workspace A";
+            alt-s = "workspace S";
+            "alt-shift-1" = ["move-node-to-workspace 1" "workspace 1"];
+            "alt-shift-2" = ["move-node-to-workspace 2" "workspace 2"];
+            "alt-shift-3" = ["move-node-to-workspace 3" "workspace 3"];
+            "alt-shift-w" = ["move-node-to-workspace W" "workspace W"];
+            "alt-shift-e" = ["move-node-to-workspace E" "workspace E"];
+            "alt-shift-a" = ["move-node-to-workspace A" "workspace A"];
+            "alt-shift-s" = ["move-node-to-workspace S" "workspace S"];
+            alt-q = "exec-and-forget kitty -1 -d ~/";
+            alt-x = "close";
+            alt-d = "exec-and-forget fzfmenu";
+            alt-m = "reload-config";
+            "alt-semicolon" = ["mode arrangement"];
+          };
+        };
+        arrangement = {
+          binding = {
+            esc = ["reload-config" "mode main"];
+            h = "join-with left";
+            j = "join-with down";
+            k = "join-with up";
+            l = "join-with right";
+          };
+        };
+      };
 
-      on-focused-monitor-changed = ['move-mouse monitor-lazy-center']
+      workspace-to-monitor-force-assignment = {
+        "1" = 1;
+        "2" = [2 1];
+        "3" = [2 1];
+        W = 1;
+        E = [2 1];
+        A = 1;
+        S = [2 1];
+      };
 
-      automatically-unhide-macos-hidden-apps = true
+      "on-window-detected" = [
+        {
+          "if".window-title-regex-substring = "^fzfmenu$";
+          run = ["layout floating"];
+        }
+      ];
 
-      [key-mapping]
-      preset = 'qwerty'
-
-      [gaps]
-      inner.horizontal = 12
-      inner.vertical = 8
-      outer.left = 8
-      outer.bottom = 8
-      outer.top = 26
-      outer.right = 8
-
-      [mode.main.binding]
-      alt-slash = 'layout tiles accordion'
-      alt-comma = 'layout horizontal vertical'
-
-      alt-h = 'focus left'
-      alt-j = 'focus down'
-      alt-k = 'focus up'
-      alt-l = 'focus right'
-
-      alt-shift-h = 'move left'
-      alt-shift-j = 'move down'
-      alt-shift-k = 'move up'
-      alt-shift-l = 'move right'
-
-      alt-minus = 'resize smart -50'
-      alt-equal = 'resize smart +50'
-      alt-shift-minus = 'balance-sizes'
-      alt-shift-equal = 'balance-sizes'
-
-      alt-backtick = 'focus-monitor --wrap-around next'
-      alt-shift-backtick = [
-        'move-node-to-monitor --wrap-around next',
-        'focus-monitor --wrap-around next',
-      ]
-
-      alt-tab = 'exec-and-forget fzfmenu -q="fo "'
-      # alt-shift-tab = 'exec-and-forget s fzfmenu summon-window'
-
-      alt-1 = 'workspace 1'
-      alt-2 = 'workspace 2'
-      alt-3 = 'workspace 3'
-      alt-w = 'workspace W'
-      alt-e = 'workspace E'
-      alt-a = 'workspace A'
-      alt-s = 'workspace S'
-
-      alt-shift-1 = ['move-node-to-workspace 1', 'workspace 1']
-      alt-shift-2 = ['move-node-to-workspace 2', 'workspace 2']
-      alt-shift-3 = ['move-node-to-workspace 3', 'workspace 3']
-      alt-shift-w = ['move-node-to-workspace W', 'workspace W']
-      alt-shift-e = ['move-node-to-workspace E', 'workspace E']
-      alt-shift-a = ['move-node-to-workspace A', 'workspace A']
-      alt-shift-s = ['move-node-to-workspace S', 'workspace S']
-
-      alt-q = 'exec-and-forget kitty -1 -d ~/'
-      alt-x = 'close'
-      alt-d = 'exec-and-forget fzfmenu'
-      # alt-f = 'exec-and-forget s wm-float'
-      alt-m = 'reload-config'
-
-      alt-semicolon = [
-        'mode arrangement',
-      ]
-
-      [mode.arrangement.binding]
-      esc = [
-        'reload-config',
-        'mode main',
-      ]
-
-      h = 'join-with left'
-      j = 'join-with down'
-      k = 'join-with up'
-      l = 'join-with right'
-
-      [workspace-to-monitor-force-assignment]
-      "1" = 1
-      "2" = [2, 1]
-      "3" = [2, 1]
-      W = 1
-      E = [2, 1]
-      A = 1
-      S = [2, 1]
-
-      [[on-window-detected]]
-      if.window-title-regex-substring = '^fzfmenu$'
-      run = ['layout floating']
-
-      [exec]
-      inherit-env-vars = true
-
-      [exec.env-vars]
-      PATH = "${config.home.homeDirectory}/.nix-profile/bin:/usr/bin:/usr/sbin:/bin:/sbin"
-    '';
+      exec = {
+        inherit-env-vars = true;
+        env-vars.PATH = "${config.home.homeDirectory}/.nix-profile/bin:/usr/bin:/usr/sbin:/bin:/sbin";
+      };
+    };
   };
 }
+
