f160ce014e5b3de0da4e7508d8345f81931646da
inogai <inogai.sign@gmail.com>
2025-12-11 00:17:35 +0800

    feat(latex): add CVPR template packages

--- modules/latex/default.nix
+++ modules/latex/default.nix
@@ -6,7 +6,31 @@
   programs.texlive = {
     enable = true;
     extraPackages = tpkgs: {
-      inherit (tpkgs) scheme-medium amsmath booktabs times cite silence etoolbox caption url eepic xecjk environ;
+      inherit (tpkgs)
+        scheme-medium
+        # Core CTAN packages for CVPR template
+        times # times
+        tools # xspace
+        xcolor # xcolor
+        graphics # graphicx, keyval, color
+        amsmath # amsmath
+        amsfonts # amssymb
+        booktabs # booktabs
+        natbib # natbib
+        silence # silence
+        etoolbox # etoolbox
+        caption # caption, subcaption
+        url # url
+        lineno # lineno
+        # Additional packages
+        enumitem # enumitem
+        cleveref # cleveref
+        # Existing packages
+        cite
+        eepic
+        xecjk
+        environ
+        ;
     };
   };
 }
