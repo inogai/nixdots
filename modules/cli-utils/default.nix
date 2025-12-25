552f6e44f5af2bd27d8f0791e057fad9b643d46c
inogai <inogai.sign@gmail.com>
2025-12-25 22:02:55 +0800

    refactor: consolidate fzf configuration into wrapper script

--- modules/cli-utils/default.nix
+++ modules/cli-utils/default.nix
@@ -1,8 +1,23 @@
 {
   config,
+  lib,
   pkgs,
   ...
-}: {
+}: let
+  fzfOptions = [
+    "--prompt=❯"
+    ''--gutter=' ' ''
+    "--color=bg:-1,bg+:-1"
+    "--color=hl:#d8a657,hl+:#d8a657:reverse"
+    "--color=fg:#ebdbb2,fg+:#ebdbb2:reverse"
+    "--color=header:#83a598"
+    "--color=spinner:#8ec07c"
+    "--color=marker:#8ec07c,header:#83a598"
+    "--color=prompt:#89b482,pointer:#83a598"
+    "--color=info:#a89984,separator:#a89984"
+    "--color=query:#ebdbb2:regular"
+  ];
+in {
   home.packages = with pkgs; [
     git
     gnupg
@@ -11,7 +26,6 @@
     curl
     fd
     ripgrep
-    fzf
     github-cli
     trash-cli
 
@@ -28,6 +42,11 @@
 
     (writeShellScriptBin "md2pdf" ''
       exec ${./md2pdf.ts} "$@"
+    '')
+
+    (writeShellScriptBin "fzf" ''
+      export FZF_DEFAULT_OPTS="${lib.concatStringsSep " " fzfOptions}"
+      exec ${pkgs.fzf}/bin/fzf "$@"
     '')
   ];
 }
--- modules/fzfmenu/default.nix
+++ modules/fzfmenu/default.nix
@@ -5,7 +5,6 @@
   ...
 }: {
   home.packages = with pkgs; [
-    fzf
     nur.repos.inogai.fzfmenu
   ];
 
