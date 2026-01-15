4872a5ee067a733c3deb0acc1c9af11d883664b8
inogai <inogai.sign@gmail.com>
2026-01-16 00:45:10 +0800

    pkg(ray-raycast): configure with completion

--- modules/gui-apps/default.nix
+++ modules/gui-apps/default.nix
@@ -7,5 +7,10 @@
     prismlauncher
     bruno
     raycast
+    nur.repos.inogai.ray-raycast
   ];
+
+  home.file."Library/Application Support/carapace/bridges.yaml".text = ''
+    ray: cobra
+  '';
 }
